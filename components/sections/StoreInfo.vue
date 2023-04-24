1919ee30ac27a6f7c4d03c2c83068785c3599af7
Yuta Toyokawa <kppg42@gmail.com>
2023-04-24 15:48:59 +0900

    mapの埋め込み

--- components/sections/StoreInfo.vue
+++ components/sections/StoreInfo.vue
@@ -10,8 +10,13 @@ section.section
       p 〒370-0411
       p 群馬県太田市亀岡町429−2
   .map.eyecatch
+    iframe(src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3217.2138738929184!2d139.3162533!3d36.258586199999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601edee5bb986ff1%3A0x215926c6b8fd27de!2z44CSMzcwLTA0MTEg576k6aas55yM5aSq55Sw5biC5LqA5bKh55S677yU77yS77yZ4oiS77ySIOODhuOCo-ODu-ODlOOCo-ODu-OCqOODlQ!5e0!3m2!1sja!2sjp!4v1682318748616!5m2!1sja!2sjp" title="rentacar map" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade")
 </template>
 
-<style lang="sass">
-// これをやらないとGlobalなcssが効かなくなる
+<style lang="sass" scoped>
+.map
+  @include pc
+    width: 100%
+    max-width: 992px
+    margin: 0 auto
 </style>
